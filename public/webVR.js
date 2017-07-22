e78e9aadcfa673b00c3b0865238e0619c1504e31
chuy <chuy@bobby-shit.wgenhq.net>
2017-07-22 12:15:39 -0400

    remove makefile

--- public/webVR.js
+++ public/webVR.js
@@ -1,28 +1,9 @@
-function onVrViewLoad() {
-  let vrView = new VRView.Player('#vrview', {
-    image: 'https://i.imgur.com/L9FeGZI.jpg',
+var onVrViewLoad = function() {
+  var vrView = new VRView.Player('#web-vr-example-view', {
+    image: 'http://i.imgur.com/L9FeGZI.jpg',
+    width: '100%',
+    height: '100%',
   });
-
-  let vrViewTwo = new VRView.Player('#vrview-two', {
-    image: 'http://raw.githubusercontent.com/jsala1990/thelare_webvr_tutorial/master/public/assets/room-2.jpg',
-  });
-
-  let vrViewThree = new VRView.Player('#vrview-three', {
-    image: 'http://raw.githubusercontent.com/jsala1990/thelare_webvr_tutorial/master/public/assets/room-5.jpg',
-  });
-
-  // vrView.addHotspot('dining-room', {
-  //   pitch: 30, // In degrees. Up is positive.
-  //   yaw: 20, // In degrees. To the right is positive.
-  //   radius: 0.05, // Radius of the circular target in meters.
-  //   distance: 2, // Distance of target from camera in meters.
-  // });
-
-  // vrView.on('click', function(event) {
-  //   if (event.id == 'dining-room') {
-  //     // Handle hotspot click.
-  //   }
-  // });
 }
 
 window.addEventListener('load', onVrViewLoad);
