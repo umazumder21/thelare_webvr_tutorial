4e930b81fc3edbd0f9ae5cb683b45c20d27793a6
chuy <chuy@chuys-MacBook-Pro.local>
2017-07-29 01:21:28 -0400

    adding day one

---
+++ public/webVRDayOne.js
@@ -0,0 +1,11 @@
+var thisIsARandomVariable = 'someString';
+
+console.log('Where is this printing out?');
+
+var thisIsARandomFunction = function() {
+  alert('Im a random function!'); // Call using the browser
+};
+
+var printMyName = function(name) {
+  alert('Hello ' + name + '!');
+}
