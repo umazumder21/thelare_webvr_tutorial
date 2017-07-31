40217cb7d2ef18e989a996b0f8f50cad60e766dc
chuy <chuy@chuys-MacBook-Pro.local>
2017-07-31 16:13:17 -0400

    adding another hotspot

--- public/webVRDayTwo.js
+++ public/webVRDayTwo.js
@@ -12,7 +12,7 @@ var setReadyEvents = function(event) {
   vrHomePage.addHotspot('hotspot-1', {
     pitch: 0,
     yaw: 0,
-    radius: 0.05,
+    radius: 0.10,
     distance: 1
   });
 
@@ -20,14 +20,14 @@ var setReadyEvents = function(event) {
   vrHomePage.addHotspot('hotspot-2', {
     pitch: 0,
     yaw: 10,
-    radius: 0.05,
+    radius: 0.10,
     distance: 1
   });
 };
 
 // create function on click, pass in event
 var setOnClickEvents = function(event) {
-  // if you are a hotspot named 'hotspot-1', print alert
+  // if you are a hotspot named 'hotspot-1', call alert
   if (event.id == 'hotspot-1') {
     alert('I CLICKED!');
   }
@@ -38,6 +38,15 @@ var setOnClickEvents = function(event) {
       image: 'assets/dolphins.jpg',
       is_stereo: true,
       is_autopan_off: true,
+    });
+
+    vrHomePage.on('ready', function(event) {
+      vrHomePage.addHotspot('hotspot-back', {
+        pitch: 0,
+        yaw: 0,
+        radius: 0.10,
+        distance: 1
+      });
     });
   }
 };
@@ -56,6 +65,10 @@ vrHomePage.on('click', function(event) {
 vrHomePage.on('error', function(errorEvent) {
   console.log('there was an error', errorEvent);
 });
+
+var onGetPosition = function(coordinates) {
+  console.log(coordinates);
+}
 
 // when you finished loading the page, run VR code
 window.addEventListener('load', vrHomePage);
