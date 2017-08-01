b00d6329404ea0e9c0dccdde5697d1af4828034b
chuy <chuy@chuys-MacBook-Pro.local>
2017-08-01 00:27:44 -0400

    changing title for index-example

--- public/theLareDrawingExample.js
+++ public/theLareDrawingExample.js
@@ -1,6 +1,6 @@
-var vrHomePage = vRViewPlayer('#example-drawing-room', {
+var drawingRoom = vRViewPlayer('#example-drawing-room', {
   image: 'assets/example-rectangular-room.png',
   is_stereo: false,
 });
 
-window.addEventListener('load', vrHomePage);
+window.addEventListener('load', drawingRoom);
